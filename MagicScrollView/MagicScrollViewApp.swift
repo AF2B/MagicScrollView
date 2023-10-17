77d3e6338194e815940307362c24fbdc0588c864
andre.borba <dev_andre.filipe@outlook.com>
2023-10-17 00:22:25 -0300

    Initial Commit

---
+++ MagicScrollView/MagicScrollViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  MagicScrollViewApp.swift
+//  MagicScrollView
+//
+//  Created by André Filipe Fonsêca Borba on 17/10/23.
+//
+
+import SwiftUI
+
+@main
+struct MagicScrollViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
